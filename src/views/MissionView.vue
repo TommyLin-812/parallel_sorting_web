3df451bdc68bb1c3f66f67a763fe5b573ce64973
lintengbo <tommylintb@icloud.com>
2023-05-21 11:08:15 +0800

    提交版本

--- src/views/MissionView.vue
+++ src/views/MissionView.vue
@@ -1,31 +1,36 @@
 <template>
-    <div>
-        <h1>待排序数据</h1>
-        <el-pagination layout="total, prev, pager, next, jumper" background :page-size="50" :total="10000000"
-                       style="margin: 10px 0" @current-change="handleCurrentChange"></el-pagination>
-        <el-table :data="tableData" border style="width: 50%">
-            <el-table-column prop="num" label="数据"></el-table-column>
-        </el-table>
-    </div>
+  <div>
+    <h1>待排序数据</h1>
+    <el-pagination layout="total, prev, pager, next, jumper" background :page-size="50" :total="10000000"
+                   style="margin: 10px 0" @current-change="handleCurrentChange"></el-pagination>
+    <el-table :data="tableData" border style="width: 50%">
+      <el-table-column prop="num" label="数据"></el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 
 export default {
-    name: "MissionView",
-    data() {
-        return {
-            tableData: []
-        };
-    },
-    methods: {
-        handleCurrentChange(val) {
-            axios.get("http://localhost:8081/mission/"+val).then((result)=>{
-                this.tableData=result.data.data;
-            })
-        }
+  name: "MissionView",
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  methods: {
+    handleCurrentChange(val) {
+      axios.get("http://localhost:8081/mission/" + val).then((result) => {
+        this.tableData = result.data.data;
+      })
     }
+  },
+  mounted() {
+    axios.get("http://localhost:8081/mission/" + 1).then((result) => {
+      this.tableData = result.data.data;
+    });
+  }
 }
 </script>
 
--- src/views/TestDataView.vue
+++ src/views/TestDataView.vue
@@ -1,31 +1,32 @@
 <template>
-    <div>
-        <el-container>
-            <el-header>
-                <el-form :inline="true">
-                    <el-form-item label="数据规模">
-                        <el-select v-model="dataQty" :loading="selectLoading">
-                            <el-option
-                                    v-for="item in options"
-                                    :key="item.value"
-                                    :label="item.label"
-                                    :value="item.value">
-                            </el-option>
-                        </el-select>
-                    </el-form-item>
+  <div>
+    <el-container>
+      <el-header>
+        <el-form :inline="true">
+          <el-form-item label="数据规模">
+            <el-select v-model="dataQty" :loading="selectLoading">
+              <el-option
+                  v-for="item in options"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+              </el-option>
+            </el-select>
+          </el-form-item>
 
-                    <el-form-item>
-                        <el-button type="primary" @click="dataVisualize" :loading="loading">查看数据图表</el-button>
-                    </el-form-item>
-                </el-form>
-            </el-header>
+          <el-form-item>
+            <el-button type="primary" @click="dataVisualize" :loading="loading">查看数据图表</el-button>
+          </el-form-item>
+        </el-form>
+      </el-header>
 
-            <!--用于放置图表-->
-            <el-main>
-                <div id="main" style="width: 800px;height:500px;"></div>
-            </el-main>
-        </el-container>
-    </div>
+      <!--用于放置图表-->
+      <el-main>
+        <div id="main" style="width: 800px;height:500px;"></div>
+        <div v-if="bestThreadNum>0">对于该情况比较合适的线程数量为{{ bestThreadNum }}</div>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
@@ -33,63 +34,68 @@ import axios from "axios";
 import * as echarts from 'echarts';
 
 export default {
-    name: "TestDataView",
-    data() {
-        return {
-            selectLoading: true,
-            loading: false,
-            dataQty: '',
-            options: [],
-            dataset: [],
-            chart: ''
-        }
-    },
-    mounted() {
-        axios.get("http://localhost:8081/dataqty").then((result) => {
-            console.log(result.data);
-            let i;
-            for (i = 0; i < result.data.data.length; i++) {
-                this.options.push({value: result.data.data[i], label: result.data.data[i]});
-            }
-            this.selectLoading = false;
-
-            this.chart = echarts.init(document.getElementById('main'));
-        });
-    },
-    methods: {
-        dataVisualize() {
-            this.loading = true;
-            this.dataset = [];
-            axios.get("http://localhost:8081/testdata/" + this.dataQty).then((result) => {
-                console.log(result.data);
-                let i;
-                for (i = 0; i < result.data.data.length; i++) {
-                    this.dataset.push({
-                        '线程数量': result.data.data[i].threadNum,
-                        '执行时间': result.data.data[i].costTime
-                    })
-                }
-
-                const option = {
-                    legend: {},
-                    tooltip: {},
-                    dataset: {
-                        source: this.dataset
-                    },
-                    xAxis: {type: 'category', name: '线程数量'},
-                    yAxis: {type: 'value', name: '执行时间'},
-                    series: [{type: 'line'}]
-                };
-
-                this.chart.setOption(option);
-                this.loading = false;
-                this.$message({
-                    message: '已获取实验数据',
-                    type: 'success'
-                });
-            });
-        }
+  name: "TestDataView",
+  data() {
+    return {
+      selectLoading: true,
+      loading: false,
+      dataQty: '',
+      options: [],
+      chart: '',
+      bestThreadNum: 0,
+      minCostTime: 0
     }
+  },
+  mounted() {
+    axios.get("http://localhost:8081/dataqty").then((result) => {
+      console.log(result.data);
+      let i;
+      for (i = 0; i < result.data.data.length; i++) {
+        this.options.push({value: result.data.data[i], label: result.data.data[i]});
+      }
+      this.selectLoading = false;
+
+      this.chart = echarts.init(document.getElementById('main'));
+    });
+  },
+  methods: {
+    dataVisualize() {
+      this.loading = true;
+      axios.get("http://localhost:8081/testdata/" + this.dataQty).then((result) => {
+        console.log(result.data);
+        const option = {
+          legend: {},
+          tooltip: {},
+          xAxis: {type: 'category', name: '线程数量', data: [1,2,3,4,5,6,7,8,9,10]},
+          yAxis: {type: 'value', name: '执行时间'},
+          series: [{type: 'line', data: []}]
+        };
+
+        let i;
+        for (i = 0; i < result.data.data.length; i++) {
+          if (i === 0) this.minCostTime = result.data.data[i].costTime;
+          else if (result.data.data[i].costTime < this.minCostTime) {
+            this.minCostTime = result.data.data[i].costTime;
+            this.bestThreadNum = result.data.data[i].threadNum;
+          }
+
+          option.series[0].data.push({
+            name: result.data.data[i].threadNum,
+            value: result.data.data[i].costTime
+          })
+        }
+
+        option.series[0].data[this.bestThreadNum-1].itemStyle={color: '#00ff00'};
+
+        this.chart.setOption(option);
+        this.loading = false;
+        this.$message({
+          message: '已获取实验数据',
+          type: 'success'
+        });
+      });
+    }
+  }
 }
 </script>
 
